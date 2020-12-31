4576e49a1bd5c9b5eb6857e9e3488784a068ec0d
imsanjoykb <sanjoy.eee32@gmail.com>
2020-12-31 17:07:12 +0600

    MySQl-6

---
+++ Database Connection/MySQL Fo DS/3.8.0_indexes_exercises.sql
@@ -0,0 +1,28 @@
+/*	==================================================
+	
+    3.8.0 Index Exercises
+    
+    Ednalyn C. De Dios
+    2019-02-20
+    
+    MySQL
+    Ada Cohort
+    Codeup Data Science Career Accelerator Program
+
+	==================================================
+*/
+
+-- USE your employees database.
+USE employees;
+
+SHOW TABLES;
+
+-- DESCRIBE each table and inspect the keys and see which columns have indexes and keys.
+
+DESCRIBE departments;
+DESCRIBE dept_emp;
+DESCRIBE dept_manager;
+DESCRIBE employees;
+DESCRIBE salaries;
+DESCRIBE titles;
+    
